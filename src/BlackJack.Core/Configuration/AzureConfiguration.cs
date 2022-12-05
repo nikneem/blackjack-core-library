fa8e132acc8b0bdbdb70653f868fa6a5bb009892
Eduard <e.keilholz@4dotnet.nl>
2022-12-05 15:26:30 +0100

    Adding event grid topic config

--- src/BlackJack.Core/Configuration/AzureConfiguration.cs
+++ src/BlackJack.Core/Configuration/AzureConfiguration.cs
@@ -3,9 +3,10 @@
 public class AzureConfiguration
 {
     public const string SectionName = "Azure";
-    public string StorageAccount { get; set; } = default!;
-    public string StorageKey { get; set; } = default!;
-    public string WebPubSub { get; set; } = default!;
-    public string BlackJackHub { get; set; } = default!;
-    public string ServiceBus { get; set; } = default!;
+    public string StorageAccount { get; set; } = null!;
+    public string StorageKey { get; set; } = null!;
+    public string WebPubSub { get; set; } = null!;
+    public string BlackJackHub { get; set; } = null!;
+    public string ServiceBus { get; set; } = null!;
+    public string EventGridTopicEndpoint { get; set; } = null!;
 }
